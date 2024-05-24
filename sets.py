16a415ed51366fe7471315a8410f56c77ebdf537
Anika-Sasaki <162721789+Anika-Sasaki@users.noreply.github.com>
2024-05-24 12:31:52 -0300

    Entrego sets.py

--- sets.py
+++ sets.py
@@ -10,7 +10,7 @@ def clean_ingredients(dish_name, dish_ingredients):
     return clean
 
 def check_drinks(drink_name, drink_ingredients):
-    if ALCOHOLS in drink_ingredients:
-        return str(drink_name) + " " + "Mocktail"
-    else:
-        return str(drink_name) + " " + "Cocktail"
+    for alcohol in ALCOHOLS:
+        if alcohol in drink_ingredients:
+            return str(drink_name) + " " + "Cocktail"
+    return str(drink_name) + " " + "Mocktail"
